84f36e9fa1c0119fff1a9f14a6970576ea1a36a5
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-06-13 19:20:27 +0530

    ##

--- .vscode/main.ts
+++ .vscode/main.ts
@@ -5,13 +5,5 @@ var a = [];
 for(;;){
     document.write("Maopplx ",a[1/0]);
 }
-class state{
-    public script(){
-        console.log("error");
-    }
-    constructor(para){
-        return this.script(24.2466) / 0;
-    }
-}
 
 export default state;
